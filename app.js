7adb017549e8c13dc0cbf79cfe9fc35d33272290
Anuj Chatterjee <anujchatterjee@gmail.com>
2020-11-17 01:12:43 -0800

    Generating a html page

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 // Portfolio 
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// ES6
+const[name,github] = profileDataArgs;
+//  Old JS
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
 
-const printProfileData = (profileDataArr) => {
 
-    //ES6
-    profileDataArr.forEach((profileItem)=> console.log(profileItem));
-    // Old JS
-//     for (let i = 0; i < profileDataArr.length; i++){
-//         console.log(profileDataArr[i]);
-//     }
-};
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
 
-printProfileData(profileDataArgs);
+// console.log(name, github);
+
+// console.log(generatePage(name,github));
+
+// const printProfileData = (profileDataArr) => {
+
+//     //ES6
+//     profileDataArr.forEach((profileItem)=> console.log(profileItem));
+//     // Old JS
+// //     for (let i = 0; i < profileDataArr.length; i++){
+// //         console.log(profileDataArr[i]);
+// //     }
+// };
+
+// printProfileData(profileDataArgs);
 
 //Examples 
 
